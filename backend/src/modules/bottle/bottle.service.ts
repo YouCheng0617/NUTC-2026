a0e02515b0acff79a3e2a2715603a2fd458b53ee
yogurt0617 <20070617TimmyAlan@gmail.com>
2026-06-04 15:00:05 +0800

    新增「我的瓶子查看功能」

--- backend/src/modules/bottle/bottle.service.ts
+++ backend/src/modules/bottle/bottle.service.ts
@@ -1,5 +1,17 @@
 import prisma from "../../lib/prisma.js";
+import { PrismaClient } from "../../generated/prisma/index.js";
+const prismaClient = new PrismaClient();
+import dotenv from "dotenv";
 
-interface CreateBottleInput {
+export const getMybottles = async (memberId: number) => {
+    const myBottles = await prismaClient.bottle.findMany({
+        where: {
+            member_id: memberId,
+        },
+        orderBy: {
+            created_at: "desc",
+        }
+    });
 
+    return myBottles;
 }
