879846ece942a819e5d9c593704ab3bee16d9688
yogurt0617 <20070617TimmyAlan@gmail.com>
2026-07-24 00:48:27 +0800

    fix:便利貼靜態路徑被動態路徑覆蓋的問題

--- backend/src/modules/game/game.router.ts
+++ backend/src/modules/game/game.router.ts
@@ -5,10 +5,11 @@ const gameControllerInstance = new GameController();
 
 export function gameRouter() {
     const router = Router();
+    router.post("/daily-note", authCheck, gameControllerInstance.postDailyNote.bind(gameControllerInstance))
+    router.get("/daily-note", optionalAuthCheck, gameControllerInstance.getDailyNote.bind(gameControllerInstance))
+
     router.get("/:gameName/:difficulty/ranking", optionalAuthCheck, gameControllerInstance.getAllGameRecordsController.bind(gameControllerInstance))
     router.post("/:gameName", authCheck, gameControllerInstance.postGameRecord.bind(gameControllerInstance))
     router.get("/:gameName/:difficulty", authCheck, gameControllerInstance.getHighestRecordController.bind(gameControllerInstance))
-    router.post("/daily-note", authCheck, gameControllerInstance.postDailyNote.bind(gameControllerInstance))
-    router.get("/daily-note", optionalAuthCheck, gameControllerInstance.getDailyNote.bind(gameControllerInstance))
     return router;
 }
